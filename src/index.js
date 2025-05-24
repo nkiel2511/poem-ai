4818936b0cef712bfafc261d23f03cf6451ed276
nkiel2511 <natalie.kiel@btinternet.com>
2025-05-24 17:07:29 +0100

    Added the API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
+  let poemResult = response.data.answer;
 
   new Typewriter("#poem", {
-    strings: "Rabbie Burns poem",
+    strings: poemResult,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let input = document.querySelector("#input");
+
+  let apiKey = "7ff3c5ef4330987abab23faeo62t9ee4";
+  let prompt = `Generate scottish poem about ${input.value}`;
+  let context =
+    "Must be short and sweet, max 4 lines. Only return me the poem itself, no other wording should be returned.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemForm = document.querySelector("#form");
